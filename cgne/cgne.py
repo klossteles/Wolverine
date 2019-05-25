0511bb2ed7cfe72490f78a254c114e5887db051b
Thyago Stall <thstall@gmail.com>
2019-05-25 17:30:18 -0300

    More tests with CGNE

--- cgne/cgne.py
+++ cgne/cgne.py
@@ -12,18 +12,26 @@ import numpy as np
 
 
 def main():
-    print('Getting g')
-    g = np.memmap('./g-1.txt', mode='r+', dtype='float', shape=(1, 50816))
-    # print(g)
-    print('Getting H')
+#    g = np.random.uniform(low=0.0, high=100000, size=(20, 1))
+#    g = np.memmap('./g-1.txt', mode='r+', dtype=np.float64, shape=(50816, 1))
+    g = np.loadtxt(open('./g-test.txt', 'r'), delimiter=',')
 
-    H = np.memmap('./H-1.txt', mode='r+', dtype='float', shape=(50816, 3600))
-    print(H)
+#    H = np.random.uniform(low=0.0, high=100000, size=(20, 3))
+#    H = np.memmap('./H-1.txt', mode='r+', dtype=np.float64, shape=(50816, 3600))
+    H = np.loadtxt(open('./H-test.txt', 'r'), delimiter=',')
 
     r = g
-    trs = np.transpose(H)
-    p = np.multiply(trs, r)
-    print(p)
+    p = np.matmul(np.transpose(H), r)
+    print(H)
+    print(g)
+#    print(p)
 
+    alpha0 = np.matmul(np.transpose(r), r)
+    alpha1 = np.matmul(np.transpose(p), p)
+    print(alpha0)
+    print(alpha1)
+
+    alpha = np.divide(alpha0, alpha1)
+    print(alpha)
 
 main()
